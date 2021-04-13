4a3e523b6d38029f4df80fd7fab15c13c98bc569
SeoDongWoo1216 <dw6642@naver.com>
2021-04-13 12:35:13 +0900

    210413 12:35 DB연동, INSERT 쿼리문 추가

---
+++ 210409_DevHelloAspNet/210413_DBHandlingWebApp/FrmDataReader.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FrmDataReader.aspx.cs" Inherits="_210413_DBHandlingWebApp.FrmDataReader" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>데이터리더 사용</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Literal ID="LblTable" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ 210409_DevHelloAspNet/210413_DBHandlingWebApp/FrmDbConn.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _210413_DBHandlingWebApp
+{
+    public partial class FrmDbConn : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnConn_Click(object sender, EventArgs e)
+        {
+            // Sql데이터 그리드 뷰
+            // Sql커맨드? 게시판
+            var connString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed) conn.Open();    // 연결상태가 닫혔으면 연결상태를 열어준다.
+
+                // LblResult.Text = conn.State.ToString();
+                var query = @"Insert Memos 
+                              Values
+                              ('서동우', 'dw6642@naver.com','서동우입니다.', GetDate(), '127.0.0.1')";
+
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.ExecuteNonQuery();
+
+                    LblResult.Text = "데이터 저장완료";
+
+                }
+                catch (Exception ex)
+                {
+                    LblResult.Text = $"오류떴지롱~ : {ex}";
+
+                }
+
+
+            }
+        }
+    }
+}
