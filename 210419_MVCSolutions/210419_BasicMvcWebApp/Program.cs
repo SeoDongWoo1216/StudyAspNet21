964ec211117778d9125a892aa5563d003559bf27
SeoDongWoo1216 <dw6642@naver.com>
2021-04-19 11:43:07 +0900

    210419 MVC 모델 샘플만들기

---
+++ 210419_MVCSolutions/210419_BasicMvcWebApp/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _210419_BasicMvcWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)  // 진입점(엔트리 포인트)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
