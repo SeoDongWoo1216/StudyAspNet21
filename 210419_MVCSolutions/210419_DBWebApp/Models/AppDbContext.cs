b227a34d9ab8aa2cf9b643c5dadbc14d81a1a96d
SeoDongWoo1216 <dw6642@naver.com>
2021-04-19 17:00:19 +0900

    0419 웹과 DB 연동 샘플

---
+++ 210419_MVCSolutions/210419_DBWebApp/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _210419_DBWebApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Speaker> Speakers { get; set; }
+
+    }
+}
---
+++ 210419_MVCSolutions/210419_DBWebApp/Models/Room.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _210419_DBWebApp.Models
+{
+
+    
+    public class Room // 부모
+    {
+        [Key]  // DB연동
+        public int RoomId { get; set; }
+
+        [Required]  // NotNull
+        public string Name { get; set; }
+        public virtual ICollection<Speaker> Speakers { get; set; }
+    }
+
+
+   
+    public class Speaker  // 자식
+    {
+        [Key]
+        public int SpeakerId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int RoomId { get; set; }
+        public virtual Room Room { get; set; }
+    }
+}
+
