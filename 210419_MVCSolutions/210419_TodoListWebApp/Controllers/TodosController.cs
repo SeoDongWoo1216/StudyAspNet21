071877c879ddfb262dce641618b40c8721c577b8
SeoDongWoo1216 <dw6642@naver.com>
2021-04-19 17:05:46 +0900

    0419 MVC 프로젝트 : EntityFramework와 DB연동, 할일 게시판 리스트 출력 및 일정 완료 처리

---
+++ 210419_MVCSolutions/210419_TodoListWebApp/Controllers/TodosController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using _210419_TodoListWebApp.Models;
+
+namespace _210419_TodoListWebApp.Controllers
+{
+    public class TodosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TodosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Todos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Todos.ToListAsync());
+        }
+
+        //// GET: Todos/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var todo = await _context.Todos
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (todo == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(todo);
+        //}
+
+        // GET: Todos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Todos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title")] Todo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                todo.IsDone = false;
+                todo.CreationDate = DateTime.Now;
+
+                _context.Add(todo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(todo);
+        }
+
+        // GET: Todos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var todo = await _context.Todos.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            return View(todo);
+        }
+
+        // POST: Todos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,IsDone,CreationDate")] Todo todo)
+        {
+            if (id != todo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    todo.CreationDate = DateTime.Now;
+                    _context.Update(todo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TodoExists(todo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(todo);
+        }
+
+
+        public async Task<IActionResult> Done(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var todo = await _context.Todos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            todo.IsDone = true;
+            _context.Todos.Update(todo);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        //// POST: Todos/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    var todo = await _context.Todos.FindAsync(id);
+        //    _context.Todos.Remove(todo);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+
+        private bool TodoExists(int id)
+        {
+            return _context.Todos.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ 210419_MVCSolutions/210419_TodoListWebApp/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _210419_TodoListWebApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)   // 이게 있어야 DB랑 연계됨
+        {
+
+        }
+
+        public DbSet<Todo> Todos {get; set;}
+    }
+}
