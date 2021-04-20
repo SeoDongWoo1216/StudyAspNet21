0861671224b5206a1b41897a8e8ee2a3e99fc5c5
SeoDongWoo1216 <dw6642@naver.com>
2021-04-20 13:55:28 +0900

    0420 Web API 샘플 테스트

---
+++ 210419_MVCSolutions/210420_RestApiTestApp/Data/_210420_AbbDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using _210420_RestApiTestApp.Models;
+
+namespace _210420_RestApiTestApp.Data
+{
+    public class _210420_AbbDbContext : DbContext
+    {
+        public _210420_AbbDbContext (DbContextOptions<_210420_AbbDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
---
+++ 210419_MVCSolutions/210420_RestApiTestApp/Startup.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using _210420_RestApiTestApp.Data;
+
+namespace _210420_RestApiTestApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "_210420_RestApiTestApp", Version = "v1" });
+            });
+
+            services.AddDbContext<_210420_AbbDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("_210420_AbbDbContext")));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "_210420_RestApiTestApp v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
