b49aaaaa82c8d7aaa4955c0e739a13d1b918067b
SeoDongWoo1216 <dw6642@naver.com>
2021-04-21 17:26:13 +0900

    0421 Contact(연락바람) 추가

---
+++ 210419_MVCSolutions/210420_MyPortpolio/Controllers/ContactsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using _210420_MyPortpolio.Data;
+using _210420_MyPortpolio.Models;
+
+namespace _210420_MyPortpolio.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContactsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        // GET: Contacts
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index([Bind("Id, Name, Email, Contents")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    contact.RegDate = DateTime.Now;
+                    _context.Add(contact);
+                    await _context.SaveChangesAsync();
+
+                    // Back단에있는 문자열을 View 화면쪽으로 옮기기위해 쓰는 클래스 중 하나
+                    ViewBag.Message = "감사합니다. 연락드리겠습니다.";
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = $"예외가 발생했습니다. {ex.Message}";
+                }
+                // return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+    }
+}
---
+++ 210419_MVCSolutions/210420_MyPortpolio/Models/Contact.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _210420_MyPortpolio.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "이름은 필수입니다")]
+        [DataType(DataType.Text)]
+        [StringLength(50)]
+        public string Name { get; set; }
+        
+
+        [Required(ErrorMessage = "이메일은 필수입니다")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(125)]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "내용은 필수입니다")]
+        [DataType(DataType.Text)]
+        public string Contents { get; set; }
+        
+
+        [DataType(DataType.Date)]
+        public DateTime RegDate { get; set; }
+
+    }
+}
