3f3b9d1d9c8d01c2c87b4a5db4c4425771f01aff
SeoDongWoo1216 <dw6642@naver.com>
2021-04-26 17:10:55 +0900

    0426 나만의 포트폴리오 만들기

---
+++ 210419_MVCSolutions/MyPortFolio/Models/Manage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortFolio.Models
+{
+    public class Manage
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "제목은 필수입니다")]
+        [DataType(DataType.Text)]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "카테고리는 필수입니다")]
+        [DataType(DataType.Text)]
+        public string Category { get; set; }
+
+
+        [Required(ErrorMessage = "내용은 필수입니다")]
+        [DataType(DataType.Text)]
+        public string Contents { get; set; }
+
+
+        [DataType(DataType.DateTime)]
+        public DateTime RegDate { get; set; }
+    }
+}
